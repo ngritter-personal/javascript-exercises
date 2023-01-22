47d985837b2908c15a45268b9fe81d20d4bece01
ngritter <ngritter44@gmail.com>
2023-01-22 10:28:46 -0800

    Lesson 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,31 @@
 const removeFromArray = function() {
 
-};
+    let arrayInput = arguments[0];
+
+    let newArray = [];
+
+    outer:
+    for (i=0; i <= arrayInput.length; i++) {
+        
+        for (j=1; j <= arguments.length; j++) {
+
+            if (arrayInput[i] === arguments[j]) {
+                continue outer;
+            }
+
+        }
+        
+        newArray.push(arrayInput[i]);
+    }   
+
+    return newArray;
+
+}
+
+
+let test = [1, 2, 3, 4];
+
+console.log(removeFromArray(test, 1, 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
