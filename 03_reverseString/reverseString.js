bf14e9b8ddb65fe6bb2f0ab225423ee95673392a
ngritter <ngritter44@gmail.com>
2023-01-22 09:54:10 -0800

    Third lesson complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let forwardArray = string.split('');
+    let backwardArray = [];
+
+    for (i = forwardArray.length; i >= 0; i--) {
+        backwardArray.push(forwardArray[i]);
+    }
+
+    backwardArray.shift(); //remove undefined element from beginning
+
+    let backwardoutput = backwardArray.join("");
+
+    return backwardoutput;
 
 };
+
+console.log(reverseString('test'));
 
 // Do not edit below this line
 module.exports = reverseString;
