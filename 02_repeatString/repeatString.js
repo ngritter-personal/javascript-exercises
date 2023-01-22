b1dfaff9c95b63dda4ed96b6bee5eaa208afe78b
ngritter <ngritter44@gmail.com>
2023-01-22 09:23:10 -0800

    Second lesson complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+
+    let output = '';
+    for (let i = 0; i < num; i++) {
+        output += string;
+    }
+
+    return output;
 
 };
 
