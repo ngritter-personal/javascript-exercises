a2d5df6a3e432e1145e10257f602efafe76d2194
ngritter <ngritter44@gmail.com>
2023-01-26 22:12:58 -0800

    Final lesson complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+
+  let celsius = (input - 32)*(5/9);
+  let celsius_output = Math.round(10*celsius)/10;
+
+  return celsius_output;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+
+  let fareinheit = (input*(9/5))+32;
+  let fareinheit_output = Math.round(10*fareinheit)/10;
+
+  return fareinheit_output;
+
 };
+
+console.log(convertToFahrenheit(1.7));
 
 // Do not edit below this line
 module.exports = {
