77772d0008589adb896afd0d69cceb3f150ada22
ngritter <ngritter44@gmail.com>
2023-02-13 21:18:29 -0800

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let flatten = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replaceAll(' ', '');
+    let lower = flatten.toLowerCase();
+    
+    var splitString = lower.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
 
+    if (lower === joinArray) {
+        return true;
+    }
+    return false;
 };
+
+console.log(palindromes("Rats live on no evil star."));
 
 // Do not edit below this line
 module.exports = palindromes;
