8315c77f984e6349029ff17eba820a1378d42983
ngritter <ngritter44@gmail.com>
2023-02-13 20:18:41 -0800

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;  
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const sum = array.reduce(add, 0);
+  return sum;
 };
 
-const multiply = function() {
 
+const multiply = function(array) {
+  let mult_two = (a,b) => a*b;
+
+  const mult = array.reduce(mult_two, 1);
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;	
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+  if (input === 0) return 1;
+  let fact = 1;
+  for (let i = input; i >0; i--) {
+    fact *= i;
+  };
+  return fact;
+
 };
 
 // Do not edit below this line
