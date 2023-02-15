a242c88898c04750b0e14eeda1b5bbe4d5d5db55
ngritter <ngritter44@gmail.com>
2023-02-14 21:35:07 -0800

    fibonnaci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num < 1) return "OOPS"; //doesn't accept negatives
+
+    let array = [1, 1];
+
+    for (i=2; i<(num+2); i++){
+        array.push(array[i-1] + array[i-2]);
+    }
+
+    return array[num-1];
 };
+
+console.log(fibonacci(5));
 
 // Do not edit below this line
 module.exports = fibonacci;
