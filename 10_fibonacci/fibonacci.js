37c02369c97a31ca2fdb21790e28e6a518f6c8fe
ngritter <ngritter44@gmail.com>
2023-02-18 09:33:55 -0800

    Complete tget title lesson

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -11,7 +11,7 @@ const fibonacci = function(num) {
     return array[num-1];
 };
 
-console.log(fibonacci(5));
+console.log(fibonacci(0));
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,27 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    let books = [];
+    
+    for (let book in array) {
+        books.push((array[book].title));
+    }
 
+    return(books);
 };
+
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+getTheTitles(books);
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
